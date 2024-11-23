280467b1054754795105bc6ee383d5e2e38430a3
nazar2005nz <152500447+nazar2005nz@users.noreply.github.com>
2024-11-23 17:57:24 +0300

    Lab8

---
+++ main1.py
@@ -0,0 +1,26 @@
+from module import greet
+
+if __name__ == "__main__":
+    print(greet("PyCharm User"))
+import pygame
+print("Pygame version:", pygame.ver)
+
+import pygame
+
+run = True
+width = 400
+height = 100
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+font = pygame.font.SysFont(None, 48)
+text = font.render("Welcome to pygame", True, (255, 255, 255))
+screen.blit(text, ((width - text.get_width()) // 2, (height - text.get_height()) // 2))
+pygame.display.flip()
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT \
+        or event.type == pygame.MOUSEBUTTONUP \
+        or event.type == pygame.KEYUP:
+            run = False
+
+
